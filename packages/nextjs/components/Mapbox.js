476f647822144e96c910d262709718221c713a31
John X <john.iv@x25bd.com>
2024-05-02 14:51:34 +0100

    Add marker to map on click.

--- packages/nextjs/components/Mapbox.js
+++ packages/nextjs/components/Mapbox.js
@@ -1,14 +1,16 @@
-import mapboxgl from "!mapbox-gl";
 // eslint-disable-line import/no-webpack-loader-syntax
-import React, { useRef, useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import mapboxgl from "!mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 mapboxgl.accessToken = "pk.eyJ1Ijoicm9uY2h1Y2siLCJhIjoiY2x2Y2o5Z2drMGY3cjJrcGI4b2xsNzdtaCJ9.gi5RJ8qRhTSwfYuhVwhmvQ";
 export default function Mapbox() {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(-118.464182);
-  const [lat, setLat] = useState(33.982547);
-  const [zoom, setZoom] = useState(14);
+  const markersRef = useRef([]);
+  const [lng, setLng] = useState(-104.98716);
+  const [lat, setLat] = useState(39.7323862);
+  const [zoom, setZoom] = useState(15);
 
   useEffect(() => {
     if (map.current) return;
@@ -24,6 +26,32 @@ export default function Mapbox() {
     setLng(map.current.getCenter().lng.toFixed(4));
     setLat(map.current.getCenter().lat.toFixed(4));
     setZoom(map.current.getZoom().toFixed(2));
+  });
+  map.current?.on("click", e => {
+    // Clear existing markers
+    markersRef.current.forEach(marker => marker.remove());
+    markersRef.current = [];
+
+    // Set state variables
+    setLng(e.lngLat.lng);
+    setLat(e.lngLat.lat);
+
+    // Add a pin to the map
+    var newMarker = new mapboxgl.Marker({
+      color: "red",
+      // anchor: "bottom",
+    })
+      .setLngLat(e.lngLat)
+      .addTo(map.current);
+
+    // Animated flyTo to position marker at center of map
+    map.current.flyTo({
+      center: e.lngLat,
+      essential: true,
+    });
+
+    // Add to state
+    markersRef.current.push(newMarker);
   });
   return (
     <div>
