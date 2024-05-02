e62290145ee8720cf1a80ea032a6cb7895c89b6a
John X <john.iv@x25bd.com>
2024-05-02 15:42:44 +0100

    Adding check-in functionality handlers

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Mapbox from "../components/Mapbox";
 import { Address } from "../components/scaffold-eth";
 // import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
@@ -10,7 +10,15 @@ import { useAccount } from "wagmi";
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
-
+  const [isCheckInActive, setCheckInActive] = useState(false);
+  const handleCheckIn = () => {
+    console.log("Moving on to the next step!");
+    // Display check-in form
+  };
+  const cancelCheckIn = () => {
+    console.log("Canceled check-in!");
+    // Clear marker
+  };
   return (
     <>
       <div className="flex items-center flex-col w-full flex-grow pt-10">
@@ -25,13 +33,16 @@ const Home: NextPage = () => {
         </div>
 
         <div className="flex-grow center w-full">
-          <div className="my-2 p-5 bg-base-300">
+          <button className="my-2 p-5 bg-base-300" disabled={!isCheckInActive} onClick={handleCheckIn}>
             <p className="text-center text-lg">CHECK - IN</p>
-          </div>
+          </button>
+          <button className="my-2 p-5 bg-base-300" disabled={!isCheckInActive} onClick={cancelCheckIn}>
+            <p className="text-center text-lg">CANCEL</p>
+          </button>
           <div className="my-2 p-5 bg-base-300">
             <p className="text-center text-lg">CONFIDENCE LEADERBOARD</p>
           </div>
-          <Mapbox />
+          <Mapbox setCheckInActive={setCheckInActive} />
         </div>
       </div>
     </>
--- packages/nextjs/components/Mapbox.js
+++ packages/nextjs/components/Mapbox.js
@@ -4,7 +4,7 @@ import mapboxgl from "!mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 mapboxgl.accessToken = "pk.eyJ1Ijoicm9uY2h1Y2siLCJhIjoiY2x2Y2o5Z2drMGY3cjJrcGI4b2xsNzdtaCJ9.gi5RJ8qRhTSwfYuhVwhmvQ";
-export default function Mapbox() {
+export default function Mapbox({ setCheckInActive }) {
   const mapContainer = useRef(null);
   const map = useRef(null);
   const markersRef = useRef([]);
@@ -13,46 +13,50 @@ export default function Mapbox() {
   const [zoom, setZoom] = useState(15);
 
   useEffect(() => {
-    if (map.current) return;
+    if (!map.current) {
+      map.current = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: "mapbox://styles/mapbox/dark-v11",
+        center: [lng, lat],
+        zoom: zoom,
+      });
+    }
     // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/dark-v11",
-      center: [lng, lat],
-      zoom: zoom,
+
+    map.current?.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+    map.current?.on("click", e => {
+      // Clear existing markers
+      markersRef.current.forEach(marker => marker.remove());
+      markersRef.current = [];
+
+      // Set state variables
+      setLng(e.lngLat.lng);
+      setLat(e.lngLat.lat);
+
+      // Add a pin to the map
+      var newMarker = new mapboxgl.Marker({
+        color: "red",
+        // anchor: "bottom",
+      })
+        .setLngLat(e.lngLat)
+        .addTo(map.current);
+
+      // Animated flyTo to position marker at center of map
+      map.current.flyTo({
+        center: e.lngLat,
+        essential: true,
+      });
+
+      // Add to state
+      markersRef.current.push(newMarker);
+      setCheckInActive(true);
     });
   });
-  map.current?.on("move", () => {
-    setLng(map.current.getCenter().lng.toFixed(4));
-    setLat(map.current.getCenter().lat.toFixed(4));
-    setZoom(map.current.getZoom().toFixed(2));
-  });
-  map.current?.on("click", e => {
-    // Clear existing markers
-    markersRef.current.forEach(marker => marker.remove());
-    markersRef.current = [];
 
-    // Set state variables
-    setLng(e.lngLat.lng);
-    setLat(e.lngLat.lat);
-
-    // Add a pin to the map
-    var newMarker = new mapboxgl.Marker({
-      color: "red",
-      // anchor: "bottom",
-    })
-      .setLngLat(e.lngLat)
-      .addTo(map.current);
-
-    // Animated flyTo to position marker at center of map
-    map.current.flyTo({
-      center: e.lngLat,
-      essential: true,
-    });
-
-    // Add to state
-    markersRef.current.push(newMarker);
-  });
   return (
     <div>
       {/* <div className="sidebar">
