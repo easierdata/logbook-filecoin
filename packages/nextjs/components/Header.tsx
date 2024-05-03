44e77e2c1b49714174dca7177a0acffdee9220f5
ronerlih <ronerlih@gmail.com>
2024-05-03 15:12:07 +0300

    ✨ add pages and dev links

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -4,12 +4,12 @@ import React, { useCallback, useRef, useState } from "react";
 // import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { FaucetButton, RainbowKitCustomConnectButton } from "../components/scaffold-eth";
+import { useOutsideClick } from "../hooks/scaffold-eth";
 import {
   Bars3Icon,
   /*BugAntIcon*/
 } from "@heroicons/react/24/outline";
-import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
-import { useOutsideClick } from "~~/hooks/scaffold-eth";
 
 type HeaderMenuLink = {
   label: string;
@@ -27,6 +27,14 @@ export const menuLinks: HeaderMenuLink[] = [
   //   href: "/debug",
   //   icon: <BugAntIcon className="h-4 w-4" />,
   // },
+  {
+    label: "Checkin form (for dev)",
+    href: "/checkinForm",
+  },
+  {
+    label: "attestation info (for dev)",
+    href: "/attestationInfo",
+  },
 ];
 
 export const HeaderMenuLinks = () => {
