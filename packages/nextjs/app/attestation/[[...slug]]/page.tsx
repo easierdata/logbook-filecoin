5145faa198938818631dca91c5c58376142976de
ronerlih <ronerlih@gmail.com>
2024-05-09 17:32:08 +0300

    parse attestation from graphql

--- packages/nextjs/app/attestation/[[...slug]]/page.tsx
+++ packages/nextjs/app/attestation/[[...slug]]/page.tsx
@@ -1,40 +1,34 @@
 "use client";
 
-import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "next/navigation";
-import { Attestation } from "@ethereum-attestation-service/eas-sdk";
+import { useQuery } from "@apollo/client";
+import { ethers } from "ethers";
 import type { NextPage } from "next";
-import easConfig from "~~/EAS.config";
-import { EASContext } from "~~/components/EasContextProvider";
+import { GET_ATTESTATION } from "~~/services/queries";
 
 // import Link from "next/link";
 
 const CheckinFrom: NextPage = () => {
-  const { eas, isReady } = useContext(EASContext);
-  const [attestation, setAttestation] = useState<Attestation>();
   const params = useParams();
 
-  console.log("[🧪 DEBUG](pathname):", params.slug);
-  // Get attestation from EAS api
-  useEffect(() => {
-    if (!isReady) return;
-    // if UID path, pick up uid slug e.g "/attestation/uid/0xff08...cc3e"
-    const attestationUID =
-      params.slug?.length > 0 && params.slug[0] === "uid" ? params.slug[1] : easConfig.ATTESTATION_UID;
-    eas
-      .getAttestation(attestationUID) // TODO: Read attestation from url slug.
-      .then(attestation => {
-        console.log("[🧪 DEBUG](attestation):", attestation);
-        setAttestation(attestation);
-      })
-      .catch(err => {
-        console.log("[🧪 DEBUG](err):", err);
-      });
-  }, [eas, isReady]);
+  const { data } = useQuery(GET_ATTESTATION, {
+    variables: { id: params.slug?.length > 0 && params.slug[0] === "uid" ? params.slug[1] : "" },
+  });
 
+  const hexToDate = (hex: string) => {
+    const timeInSeconds = parseInt(hex, 16);
+    // convert to miliseconds
+    const timeInMiliseconds = timeInSeconds * 1000;
+    const currentTime = new Date(timeInMiliseconds).toLocaleDateString();
+    return currentTime;
+  };
   return (
     // TODO: handle error/ no attestation.
     <>
+      {console.log(
+        "[🧪 DEBUG](decodedDataJson):",
+        data?.attestation?.decodedDataJson && JSON.parse(data?.attestation?.decodedDataJson),
+      )}
       <div className="hero min-h-screen bg-base-100">
         <div className="overflow-x-auto">
           <table className="table">
@@ -48,50 +42,39 @@ const CheckinFrom: NextPage = () => {
               </tr>
             </thead> */}
             <tbody>
-              {/* row 1 */}
               <tr>
-                <td>uid</td>
-                <td>{attestation?.uid || "fetching"}</td>
+                <td>coordinate</td>
+                <td>
+                  (
+                  {(data?.attestation?.decodedDataJson &&
+                    JSON.parse(data?.attestation?.decodedDataJson)[0].value.value.toString()) ||
+                    "fetching"}
+                  )
+                </td>
               </tr>
-              {/* row 2 */}
-              <tr>
-                <td>schema</td>
-                <td>{attestation?.schema || "fetching"}</td>
-              </tr>
-              {/* row 3 */}
-              <tr>
-                <td>refUID</td>
-                <td>{attestation?.refUID || "fetching"}</td>
-              </tr>
-              {/* row 4 */}
-              <tr>
-                <td>time</td>
-                <td>{attestation?.time || "fetching"}</td>
-              </tr>
-              {/* row 5 */}
-              <tr>
-                <td>expirationTime</td>
-                <td>{attestation?.expirationTime || "fetching"}</td>
-              </tr>
-              {/* row 6 */}
-              <tr>
-                <td>revocationTime</td>
-                <td>{attestation?.revocationTime || "fetching"}</td>
-              </tr>
-              {/* row 6 */}
-              <tr>
-                <td>recipient</td>
-                <td>{attestation?.recipient || "fetching"}</td>
-              </tr>
-              {/* row 6 */}
               <tr>
                 <td>attester</td>
-                <td>{attestation?.attester || "fetching"}</td>
+                <td>
+                  {(data?.attestation?.decodedDataJson &&
+                    JSON.parse(data?.attestation?.decodedDataJson)[1].value.value.toString()) ||
+                    "fetching"}
+                </td>
               </tr>
-              {/* row 6 */}
               <tr>
-                <td>revocable</td>
-                <td>{attestation?.revocable || "fetching"}</td>
+                <td>attester</td>
+                <td>
+                  {(data?.attestation?.decodedDataJson &&
+                    hexToDate(JSON.parse(data?.attestation?.decodedDataJson)[2].value.value.hex.toString())) ||
+                    "fetching"}
+                </td>
+              </tr>
+              <tr>
+                <td>message</td>
+                <td>
+                  {(data?.attestation?.decodedDataJson &&
+                    ethers.decodeBytes32String(JSON.parse(data?.attestation?.decodedDataJson)[3].value.value)) ||
+                    "fetching"}
+                </td>
               </tr>
               {/* row 6 */}
               {/* TODO: parse attestation info (opt ethers abi decoder) */}
