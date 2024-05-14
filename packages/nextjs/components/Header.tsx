dacab4e80a9ab47ca4b3d39cdb2f1d33577b8da6
ronerlih <ronerlih@gmail.com>
2024-05-14 15:16:42 +0300

    fix mobile header, add astral svg stroke

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -68,7 +68,7 @@ export const Header = () => {
       <div className="navbar-start w-auto lg:w-1/2">
         <div className="text-center block font-bold ml-2">
           <Link href={"/"} className="flex items-center space-x-2 ml-2">
-            <Image alt="astral_sparkels" height={30} width={30} className="text-black" src={"/astral_sparkels.svg"} />
+            <Image alt="astral_sparkels" height={30} width={30} src={"/astral_sparkels.svg"} />
             <span className="text-black font-bold">Logbook</span>
           </Link>
         </div>
