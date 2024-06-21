a3eb37e34eb10784085af67ef1f8101f2c98e4b4
John X <john.iv@x25bd.com>
2024-06-21 17:19:55 +0100

    Changed to black astral sparkles svg

--- packages/nextjs/components/Loading.tsx
+++ packages/nextjs/components/Loading.tsx
@@ -6,7 +6,7 @@ const Loading = () => {
     <div className="hero min-h-screen bg-base-100 absolute z-10" style={{ opacity: 0.8 }}>
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <Image alt="loading" src="/astral_sparkels.svg" width={100} height={100} style={{ opacity: 0.8 }} />
+          <Image alt="loading" src="/astral-sparkles.svg" width={100} height={100} style={{ opacity: 0.8 }} />
         </div>
       </div>
     </div>
