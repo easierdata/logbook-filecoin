ea3ece8ef8476a99dbf52f875c83225b9dd9e3e6
John X <john.iv@x25bd.com>
2024-06-21 18:08:42 +0100

    AST-191: Animate sparkles on loading page :P

--- packages/nextjs/components/Loading.tsx
+++ packages/nextjs/components/Loading.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import Image from "next/image";
+import { ReactSVG } from "react-svg";
 
 const Loading = () => {
   return (
     <div className="hero min-h-screen bg-base-100 absolute z-10" style={{ opacity: 0.8 }}>
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <Image alt="loading" src="/astral-sparkles.svg" width={100} height={100} style={{ opacity: 0.8 }} />
+          <div
+            className="svg-container absolute top-1/4 left-1/2 -translate-x-1/2"
+            style={{ width: "200px", height: "200px" }}
+          >
+            <ReactSVG src="/astral-sparkles.svg" className="animated-svg" />
+          </div>{" "}
+          {/* <Image alt="loading" type="image/svg+xml" src="/astral-sparkles.svg" width={100} height={100} style={{ opacity: 0.8 }} /> */}
         </div>
       </div>
     </div>
