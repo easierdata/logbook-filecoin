151b7b45dda0e080e12cecebf43ef9dfa087e98b
André Silva <andre.silva@posteo.de>
2024-09-24 10:14:32 +1000

    fix: set next.config ignore commands to false

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -4,10 +4,10 @@
 const nextConfig = {
   reactStrictMode: true,
   typescript: {
-    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "false",
   },
   eslint: {
-    ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+    ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "false",
   },
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
